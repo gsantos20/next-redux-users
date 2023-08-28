759ef34a160034f03556aaa8f8e05909c4b79a22
Gustavo Neris <nerisgs20@gmail.com>
2023-08-28 03:46:02 -0300

    feat: fixing index page home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from 'react'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
+import { Provider } from 'react-redux';
+import { store } from 'store';
 
 import GlobalStyle from '../styles/global'
 import theme from '../styles/theme'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import Head from 'next/head';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   useEffect(() => {
@@ -13,10 +16,26 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   }, [])
 
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-      <GlobalStyle />
-    </ThemeProvider>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
+        <meta charSet="utf-8" />
+
+        <title>Next CRUD Users</title>
+        <link rel="shortcut icon" href="next.svg" type="image/x-icon" />
+      </Head>
+
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+          <GlobalStyle />
+        </ThemeProvider>
+      </Provider>
+    </>
+
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,12 @@
-import Head from 'next/head'
+import User from 'components/User'
 import React from 'react'
+import { Container } from 'react-bootstrap'
 
 const Home: React.FC = () => {
   return (
-    <>
-    <Head>
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-      />
-      <meta charSet="utf-8" />
-
-      <title>Next CRUD Users</title>
-      <link rel="shortcut icon" href="next.svg" type="image/x-icon" />
-    </Head>
-
-    </>
+    <Container>
+      <User/>
+    </Container>
   )
 }
 
