4aa6ae6e406311dd4f022b8abd95fa8919eca5dc
Gustavo Neris <nerisgs20@gmail.com>
2023-08-29 04:47:11 -0300

    feat: add reducers state with redux

--- src/reducers/users.ts
+++ src/reducers/users.ts
@@ -1,6 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { CreateUser, User } from "@models/User"
 import * as UserService from "@services/UserService";
+import { toastr } from "common/Toastr";
 
 const initialState: User[] = [];
 
@@ -11,51 +12,80 @@ export const usersSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(getUsers.fulfilled, (state, action) => {
-        return action.payload.data;
+        return action.payload;
       })
       .addCase(createUser.fulfilled, (state, action) => {
-        state.push(action.payload.data);
+        state.push(action.payload);
       })
       .addCase(updateUser.fulfilled, (state, action) => {
         console.log(action.payload);
-        const { id, data } = action.payload.data.id;
+        const { id, data } = action.payload.id;
         const userIndex = state.findIndex(user => user.id === id);
         if (userIndex !== -1) {
           state[userIndex] = data;
         }
       })
       .addCase(deleteUser.fulfilled, (state, action) => {
-        return state.filter(user => user.id !== action.payload.data);
+        return state.filter(user => user.id !== action.payload);
       });
   },
 });
 
-export const getUsers = createAsyncThunk('users/fetchUsers', async () => {
-  const response = await UserService.get();
-  return response.data;
+
+export const getUsers = createAsyncThunk('users/getUsers', async () => {
+  const res = await UserService.get();
+
+  if(res.data.success){
+    return res.data.data;
+  }else{
+    toastr.error(res.data)
+    throw new Error(res.data.data)
+  }
 });
 
 export const createUser = createAsyncThunk(
   'users/createUser',
   async (user: CreateUser) => {
-    const response = await UserService.create(user);
-    return response.data;
+    const res = await UserService.create(user);
+
+    if(res.data.success){
+      toastr.success('Usuário criado com sucesso!')
+      return res.data.data;
+    }else{
+      toastr.error(res.data.data)
+      throw new Error(res.data.data)
+    }
   }
 );
 
 export const updateUser = createAsyncThunk(
   'users/updateUser',
-  async ({ id, data }: { id: number; data: User }) => {
-    const response = await UserService.update(id, data);
-    return response.data;
+  async (user: User) => {
+    const { id, ...data } = user
+    const res = await UserService.update(id, data);
+
+    if(res.data.success){
+      toastr.success('Usuário atualizado com sucesso!')
+      return res.data.data;
+    }else{
+      toastr.error(res.data.data)
+      throw new Error(res.data.data)
+    }
   }
 );
 
 export const deleteUser = createAsyncThunk(
   'users/deleteUser',
   async (id: number) => {
-    const response = await UserService.remove(id);
-    return response.data;
+    const res = await UserService.remove(id);
+
+    if(res.data.success){
+      toastr.success('Usuário removido com sucesso!')
+      return res.data.data;
+    }else{
+      toastr.error(res.data.data)
+      throw new Error(res.data.data)
+    }
   }
 );
 
