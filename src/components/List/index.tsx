eef201870db4db73b29f260504b017d0b5b086d3
Gustavo Neris <nerisgs20@gmail.com>
2023-08-29 04:49:48 -0300

    feat: ajusts layout and actions in list users

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,35 +1,46 @@
 
-import { ShowUser, User } from "@models/User";
+import { CreateUser, User } from "@models/User";
 import moment from "moment";
 import { Container } from "react-bootstrap";
 
-const TableUser = ({
-  id,
-  email,
-  username,
-  firstName,
-  lastName,
-  createdAt,
-  updatedAt,
-}: ShowUser) => {
+const TableUser = ({ item, editUser, removeUser }: {
+  item: User;
+  editUser: (user: User) => void;
+  removeUser: (id: number) => void;
+}) => {
   return (
-    <tr key={id}>
-      <td>{id}</td>
-      <td>{username}</td>
-      <td>{firstName}</td>
-      <td>{lastName}</td>
-      <td>{email}</td>
-      <td>{moment(createdAt).format('DD/MM/YYYY HH:mm')}</td>
-      <td>{moment(updatedAt).format('DD/MM/YYYY HH:mm')}</td>
+    <tr key={item.id}>
+      <td>{item.id}</td>
+      <td>{item.username}</td>
+      <td>{item.firstName}</td>
+      <td>{item.lastName}</td>
+      <td>{item.email}</td>
+      <td>{moment(item.createdAt).format('DD/MM/YYYY HH:mm')}</td>
+      <td>{moment(item.updatedAt).format('DD/MM/YYYY HH:mm')}</td>
+      <td>
+      <button type="button" className="btn btn-outline-primary btn-sm"
+        onClick={() => editUser(item)}>
+        <i className="fa fa-edit"></i>
+      </button>
+      &nbsp;&nbsp;
+      <button type="button"
+        className="btn btn-outline-danger btn-sm"
+        onClick={() => removeUser(item.id)}>
+        <i className="fa fa-trash"></i>
+      </button>
+      </td>
     </tr>
   );
 };
 
 interface ListProps {
   data: User[];
+  editUser: (user: User) => void;
+  removeUser: (id: number) => void;
 }
 
-const List: React.FC<ListProps> = ({ data }) => {
+
+const List: React.FC<ListProps> = ({ data, editUser, removeUser}) => {
   return (
     <Container>
       <table className="table table-hover table-responsive">
@@ -42,19 +53,16 @@ const List: React.FC<ListProps> = ({ data }) => {
             <th>Email</th>
             <th>Data de Criação</th>
             <th>Data de Atualização</th>
+            <th>Ações</th>
           </tr>
         </thead>
         <tbody>
           {data !== null && data.map((item) => (
             <TableUser
-              id={item.id}
               key={item.id}
-              username={item.username}
-              firstName={item.firstName}
-              lastName={item.lastName}
-              email={item.email}
-              createdAt={item.createdAt}
-              updatedAt={item.updatedAt}
+              item={item}
+              editUser={editUser}
+              removeUser={removeUser}
             />
           ))}
         </tbody>
